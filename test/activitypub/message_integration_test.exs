88a88a23dbc42d942035e2329c2f838f23c89411
Karen Konou <konoukaren@gmail.com>
2021-09-08 13:40:51 +0200

    Message receive test

--- test/activitypub/message_integration_test.exs
+++ test/activitypub/message_integration_test.exs
@@ -3,6 +3,17 @@ defmodule Bonfire.Federate.ActivityPub.MessageIntegrationTest do
 
   alias Bonfire.Social.Messages
 
+  import Tesla.Mock
+
+  setup do
+    mock(fn
+      %{method: :get, url: "https://kawen.space/users/karen"} ->
+        json(Simulate.actor_json("https://kawen.space/users/karen"))
+    end)
+
+    :ok
+  end
+
   test "can federate message" do
     me = fake_user!()
     messaged = fake_user!()
@@ -10,8 +21,28 @@ defmodule Bonfire.Federate.ActivityPub.MessageIntegrationTest do
     attrs = %{circles: [messaged.id], post_content: %{html_body: msg}}
     assert {:ok, message} = Messages.send(me, attrs)
 
-    {:ok, activity} = Messages.ap_publish_activity("create", message)
+    {:ok, activity} = Bonfire.Federate.ActivityPub.Publisher.publish("create", message)
 
-    assert activity.data["object"]["content"] == msg
+    assert activity.object.data["content"] == msg
+  end
+
+  test "can receive federated messages" do
+    me = fake_user!()
+    {:ok, local_actor} = ActivityPub.Actor.get_by_local_id(me.id)
+    {:ok, actor} = ActivityPub.Actor.get_or_fetch_by_ap_id("https://kawen.space/users/karen")
+    context = "blabla"
+    object = %{"content" => "content", "type" => "ChatMessage"}
+    to = [local_actor.ap_id]
+
+    params = %{
+      actor: actor,
+      context: context,
+      object: object,
+      to: to
+    }
+
+    {:ok, activity} = ActivityPub.create(params)
+
+    assert {:ok, _message} = Bonfire.Federate.ActivityPub.Receiver.receive_activity(activity)
   end
 end
