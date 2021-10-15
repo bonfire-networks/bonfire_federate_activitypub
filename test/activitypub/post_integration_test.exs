9edf8672fb1b53afbd4dc17379a9e432f9a1bff6
Karen Konou <konoukaren@gmail.com>
2021-10-15 09:58:53 +0200

    Reply tests

--- test/activitypub/post_integration_test.exs
+++ test/activitypub/post_integration_test.exs
@@ -26,6 +26,20 @@ defmodule Bonfire.Federate.ActivityPub.PostIntegrationTest do
     assert ap_activity.object.data["content"] == attrs.post_content.html_body
   end
 
+  test "Reply publishing works" do
+    attrs = %{post_content: %{summary: "summary", name: "name", html_body: "<p>epic html message</p>"}}
+    user = fake_user!()
+    assert {:ok, post} = Posts.publish(user, attrs)
+
+    assert {:ok, original_activity} = Bonfire.Federate.ActivityPub.Publisher.publish("create", post)
+
+    attrs_reply = %{post_content: %{summary: "summary", name: "name 2", html_body: "<p>epic html message</p>"}, reply_to_id: post.id}
+    assert {:ok, post_reply} = Posts.publish(user, attrs_reply)
+
+    assert {:ok, ap_activity} = Bonfire.Federate.ActivityPub.Publisher.publish("create", post_reply)
+    assert ap_activity.object.data["inReplyTo"] == original_activity.object.data["id"]
+  end
+
   test "creates a Post for an incoming Note" do
     {:ok, actor} = ActivityPub.Actor.get_or_fetch_by_ap_id("https://kawen.space/users/karen")
     context = "blabla"
@@ -48,5 +62,37 @@ defmodule Bonfire.Federate.ActivityPub.PostIntegrationTest do
 
     # IO.inspect(post)
     assert object["content"] == post.post_content.html_body
+  end
+
+  test "creates a a reply for an incoming note with a reply" do
+    {:ok, actor} = ActivityPub.Actor.get_or_fetch_by_ap_id("https://kawen.space/users/karen")
+    context = "blabla"
+    object = %{"content" => "content", "type" => "Note"}
+    to = ["https://testing.kawen.dance/users/karen"]
+
+    params = %{
+      actor: actor,
+      context: context,
+      object: object,
+      to: to
+    }
+
+    {:ok, activity} = ActivityPub.create(params)
+
+    assert {:ok, post} = Bonfire.Federate.ActivityPub.Receiver.receive_activity(activity)
+
+    reply_object = %{"content" => "content", "type" => "Note", "inReplyTo" => activity.object.data["id"]}
+
+    reply_params = %{
+      actor: actor,
+      context: context,
+      object: reply_object,
+      to: to
+    }
+
+    {:ok, reply_activity} = ActivityPub.create(reply_params)
+
+    assert {:ok, reply} = Bonfire.Federate.ActivityPub.Receiver.receive_activity(reply_activity)
+    assert reply.replied.reply_to_id == post.id
   end
 end
