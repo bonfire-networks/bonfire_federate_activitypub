1666dad27191896434dda93b970c5b1a8fb99301
Karen Konou <konoukaren@gmail.com>
2022-01-28 12:47:32 +0100

    update test

--- test/activitypub/follow_integration_test.exs
+++ test/activitypub/follow_integration_test.exs
@@ -25,22 +25,27 @@ defmodule Bonfire.Federate.ActivityPub.FollowIntegrationTest do
   test "follow receiving works" do
     followed = fake_user!()
     {:ok, ap_follower} = ActivityPub.Actor.get_or_fetch_by_ap_id("https://kawen.space/users/karen")
+    {:ok, follower} = Bonfire.Me.Users.by_ap_id("https://kawen.space/users/karen")
     {:ok, ap_followed} = ActivityPub.Adapter.get_actor_by_id(followed.id)
     {:ok, follow_activity} = ActivityPub.follow(ap_follower, ap_followed)
 
     assert {:ok, _} = Bonfire.Federate.ActivityPub.Receiver.receive_activity(follow_activity)
+    assert Bonfire.Social.Follows.following?(follower, followed)
   end
 
   test "unfollow receiving works" do
     followed = fake_user!()
     {:ok, ap_follower} = ActivityPub.Actor.get_or_fetch_by_ap_id("https://kawen.space/users/karen")
+    {:ok, follower} = Bonfire.Me.Users.by_ap_id("https://kawen.space/users/karen")
     {:ok, ap_followed} = ActivityPub.Adapter.get_actor_by_id(followed.id)
     {:ok, follow_activity} = ActivityPub.follow(ap_follower, ap_followed)
 
     assert {:ok, _} = Bonfire.Federate.ActivityPub.Receiver.receive_activity(follow_activity)
+    assert Bonfire.Social.Follows.following?(follower, followed)
 
     {:ok, unfollow_activity} = ActivityPub.unfollow(ap_follower, ap_followed)
 
-    assert {:ok, _} = Bonfire.Federate.ActivityPub.Receiver.receive_activity(unfollow_activity)
+    Bonfire.Federate.ActivityPub.Receiver.receive_activity(unfollow_activity)
+    refute Bonfire.Social.Follows.following?(follower, followed)
   end
 end
