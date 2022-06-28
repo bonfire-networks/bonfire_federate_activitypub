536b506e62f871e0f4b9dec49ee01a7b2a5866c2
Mayel de Borniol <git@mayel.space>
2022-06-28 12:27:56 +1200

    nodeinfo

--- lib/adapter/nodeinfo_adapter.ex
+++ lib/adapter/nodeinfo_adapter.ex
@@ -6,23 +6,19 @@ defmodule Bonfire.Federate.ActivityPub.NodeinfoAdapter do
     Bonfire.Common.URIs.base_url()
   end
 
-  def user_count() do
-    # FIXME
-    1
-  end
-
   def gather_nodeinfo_data() do
     instance = Application.get_env(:activity_pub, :instance)
 
     %Nodeinfo{
       app_name: Bonfire.Application.name(),
       app_version: Bonfire.Application.version(),
-      open_registrations: Config.get([Bonfire.Me.Users, :public_registration]),
-      user_count: user_count(),
-      node_name: instance[:name],
-      node_description: instance[:description],
+      open_registrations: Bonfire.Me.Accounts.instance_is_invite_only?(),
+      user_count: Bonfire.Me.Users.count(),
+      node_name: Config.get([:ui, :theme, :instance_name], nil),
+      node_description: Config.get([:ui, :theme, :instance_description], nil),
       federating: instance[:federating],
       app_repository: Bonfire.Application.repository()
     }
   end
+
 end
