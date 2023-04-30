36df8f312e01ae68d98277b120bb9a06972d023c
Mayel de Borniol <git@mayel.space>
2023-05-01 10:06:46 +1200

    f

--- test/dance/moderation_dance_test.exs
+++ test/dance/moderation_dance_test.exs
@@ -15,68 +15,50 @@ defmodule Bonfire.Federate.ActivityPub.Dance.ModerationDanceTest do
   alias Bonfire.Social.Follows
 
   test "Ghosting a remote user works" do
-
   end
 
-
   test "Silencing a remote user works" do
-
   end
 
   describe "if I silenced a remote user i will not receive any update from it" do
     test "i'll not see anything they publish in feeds" do
-
     end
 
     test "i'll be able to view their profile or read post via direct link" do
-
     end
 
     test "i'll not see any @ mentions or DMs from them" do
-
     end
 
     test "I'll not be able to follow them" do
-
     end
 
     test "if I unsilence them i'll not be able to see previously missed updates" do
-
     end
   end
 
   describe "if I ghosted a remote user they will not be able to interact with me or with my content" do
     test "Nothing I post privately will be shown to them from now on" do
-
     end
 
     test "They will still be able to see things I post publicly. " do
-
     end
 
     test "I won't be able to @ mention or message them. " do
-
     end
 
     test "they won't be able to follow me" do
-
     end
 
     test "You will be able to undo this later but they may not be able to see any activities they missed." do
-
     end
   end
 
   describe "Admin" do
-
     test "As an admin I can ghost a remote user instance-wide" do
-
     end
 
     test "As an admin I can silence a remote user instance-wide" do
-
     end
-
   end
-
 end
