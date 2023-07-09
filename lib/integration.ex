f603e8167ea1dd30226f64edbddd240b127bba54
Mayel de Borniol <git@mayel.space>
2023-07-09 11:16:28 +0100

    f

--- lib/integration.ex
+++ lib/integration.ex
@@ -16,7 +16,11 @@ defmodule Bonfire.Federate.ActivityPub do
   end
 
   def disable() do
-    Bonfire.Me.Settings.set([activity_pub: [instance: [federating: false]]], scope: :instance, skip_boundary_check: true)
+    Bonfire.Me.Settings.set([activity_pub: [instance: [federating: false]]],
+      scope: :instance,
+      skip_boundary_check: true
+    )
+
     # Oban.cancel_all_jobs(Oban.Job)
   end
 end
