ffcc45a1a2e51158d590ed832b5ba5c1de2f0250
Mayel de Borniol <git@mayel.space>
2023-08-28 15:41:22 +0100

    account

--- lib/integration.ex
+++ lib/integration.ex
@@ -6,15 +6,6 @@ defmodule Bonfire.Federate.ActivityPub do
 
   def repo, do: Config.repo()
 
-  def is_admin?(user) do
-    if is_map(user) and Map.get(user, :instance_admin) do
-      Map.get(user.instance_admin, :is_instance_admin)
-    else
-      # FIXME
-      false
-    end
-  end
-
   def disable() do
     Bonfire.Me.Settings.set([activity_pub: [instance: [federating: false]]],
       scope: :instance,
