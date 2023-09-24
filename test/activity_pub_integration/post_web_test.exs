9bccde2f5c4bafaf10127266b5ee67225e2df6c1
Mayel de Borniol <git@mayel.space>
2023-09-24 17:18:16 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/667

--- test/activity_pub_integration/post_web_test.exs
+++ test/activity_pub_integration/post_web_test.exs
@@ -3,6 +3,7 @@ defmodule Bonfire.Federate.ActivityPub.PostIntegrationTest do
   import Tesla.Mock
   import Untangle
   alias Bonfire.Social.Posts
+  use Bonfire.Common.Repo
 
   @remote_instance "https://mocked.local"
   @remote_actor @remote_instance <> "/users/karen"
@@ -64,6 +65,43 @@ defmodule Bonfire.Federate.ActivityPub.PostIntegrationTest do
              |> put_req_header("accept", "application/activity+json")
              |> get("/post/#{post.id}")
              |> redirected_to() =~ "/pub/objects/#{post.id}"
+    end
+
+    test "pixelfed activity with image" do
+      data =
+        "../fixtures/pixelfed-image.json"
+        |> Path.expand(__DIR__)
+        |> File.read!()
+        |> Jason.decode!()
+
+      {:ok, data} = ActivityPub.Federator.Transformer.handle_incoming(data)
+
+      assert {:ok, post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(data)
+               |> repo().maybe_preload([:post_content, :media])
+
+      assert post.__struct__ == Bonfire.Data.Social.Post
+      debug(post)
+      assert is_binary(debug(e(post, :post_content, :html_body, nil)))
+
+      assert %{
+               media: [
+                 %Bonfire.Files.Media{
+                   path:
+                     "https://pixelfed-prod.nyc3.cdn.digitaloceanspaces.com/public/m/_v2/411/7198ec0c0-99bc91/6CWmVqUJS5Rx/cXZwkROZAkUOQEidxDNxZYlezi5nRBBLy5f2YAm0.jpg"
+                 }
+               ]
+             } = post
+
+      # assert doc =
+      #          render_stateful(Bonfire.UI.Social.ActivityLive, %{
+      #            id: "activity",
+      #            object: post
+      #          })
+
+      # assert doc
+      #        |> debug
+
     end
   end
 end
