d8bac2123529e37f3458404fc927cbc0e751114b
Mayel de Borniol <git@mayel.space>
2025-06-11 17:30:41 +0100

    events

--- test/activity_pub_integration/event_test.exs
+++ test/activity_pub_integration/event_test.exs
@@ -1,4 +1,4 @@
-defmodule Bonfire.Federate.ActivityPub.ActivityFallbackTest do
+defmodule Bonfire.Federate.ActivityPub.EventTest do
   use Bonfire.Federate.ActivityPub.ConnCase, async: false
   import Tesla.Mock
 
@@ -17,40 +17,48 @@ defmodule Bonfire.Federate.ActivityPub.ActivityFallbackTest do
   end
 
   describe "" do
-    test "object with Custom Type is recorded as APActivity" do
-      type = "CustomType"
+    test "event is recorded as APActivity" do
+      type = "Event"
 
       data =
-        "../fixtures/peertube-video.json"
+        "../fixtures/mobilizon.org-event.json"
         |> Path.expand(__DIR__)
         |> File.read!()
         |> Jason.decode!()
-        |> Map.put("type", type)
+
+      # |> Map.put("type", type)
 
       {:ok, data} = ActivityPub.Federator.Transformer.handle_incoming(data)
 
       assert {:ok, activity} = Bonfire.Federate.ActivityPub.Incoming.receive_activity(data)
 
+      # NOTE: temp until an extension implements events
       assert activity.__struct__ == Bonfire.Data.Social.APActivity
       assert is_map(activity.json["object"])
       assert activity.json["object"]["type"] == type
       assert is_binary(activity.json["object"]["content"])
 
       assert {:ok, _} = Bonfire.Social.Objects.read(activity.id)
+
+      feed = Bonfire.Social.FeedLoader.feed(:explore)
+      assert Bonfire.Social.FeedLoader.feed_contains?(feed, activity)
+
+      feed = Bonfire.Social.FeedLoader.feed(:explore, %{object_types: ["Event"]}, [])
+      assert Bonfire.Social.FeedLoader.feed_contains?(feed, activity)
     end
 
-    test "non-public object with Custom Type is recorded as private APActivity" do
+    test "non-public event is recorded as private APActivity" do
       recipient = fake_user!()
       recipient_actor = ActivityPub.Actor.get_cached!(pointer: recipient.id)
 
-      type = "CustomType"
+      type = "Event"
 
       data =
-        "../fixtures/peertube-video.json"
+        "../fixtures/mobilizon.org-event.json"
         |> Path.expand(__DIR__)
         |> File.read!()
         |> Jason.decode!()
-        |> Map.put("type", type)
+        # |> Map.put("type", type)
         |> Map.put("to", recipient_actor)
 
       {:ok, data} = ActivityPub.Federator.Transformer.handle_incoming(data)
