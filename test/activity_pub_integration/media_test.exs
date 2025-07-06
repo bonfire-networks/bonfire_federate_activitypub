8227fbb2030ff8ce869071b38cdaa4594bd609fa
Mayel de Borniol <git@mayel.space>
2025-07-06 18:24:30 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1411

--- test/activity_pub_integration/media_test.exs
+++ test/activity_pub_integration/media_test.exs
@@ -18,6 +18,47 @@ defmodule Bonfire.Federate.ActivityPub.MediaTest do
   end
 
   describe "supports incoming" do
+    test "pixelfed note with image" do
+      data =
+        "../fixtures/pixelfed-image.json"
+        |> Path.expand(__DIR__)
+        |> File.read!()
+        |> Jason.decode!()
+
+      # |> debug("pxxx")
+
+      {:ok, data} = ActivityPub.Federator.Transformer.handle_incoming(data)
+
+      assert {:ok, post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(data)
+               |> repo().maybe_preload([:post_content, :media])
+
+      assert post.__struct__ == Bonfire.Data.Social.Post
+      # debug(post)
+      assert is_binary(e(post, :post_content, :html_body, nil))
+
+      assert match?(
+               %{
+                 media: [
+                   %Bonfire.Files.Media{
+                     path:
+                       "https://pxscdn.com/public/m/_v2/411/7198ec0c0-99bc91/6CWmVqUJS5Rx/cXZwkROZAkUOQEidxDNxZYlezi5nRBBLy5f2YAm0.jpg"
+                   }
+                 ]
+               },
+               post
+             )
+
+      # assert doc =
+      #          render_stateful(Bonfire.UI.Social.ActivityLive, %{
+      #            id: "activity",
+      #            object: post
+      #          })
+
+      # assert doc
+      #        |> debug
+    end
+
     test "peertube video object" do
       data =
         "../fixtures/peertube-video.json"
@@ -84,6 +125,211 @@ defmodule Bonfire.Federate.ActivityPub.MediaTest do
       assert media.media_type == "audio/ogg"
 
       assert {:ok, _} = Bonfire.Social.Objects.read(media.id)
+    end
+
+    test "update activity with media changes - add primary image, remove one attachment, add another" do
+      # First, create original post based on pixelfed fixture with 2 attachments
+      second_image = %{
+        "type" => "Document",
+        "mediaType" => "image/png",
+        "url" => "https://example.com/image2.png",
+        "name" => "Second image"
+      }
+
+      original_data =
+        "../fixtures/pixelfed-image.json"
+        |> Path.expand(__DIR__)
+        |> File.read!()
+        |> Jason.decode!()
+        |> Map.put("attachment", [
+          %{
+            "type" => "Document",
+            "mediaType" => "image/jpeg",
+            "url" => "https://example.com/image1.jpg",
+            "name" => "First image"
+          },
+          second_image
+        ])
+
+      {:ok, original} = ActivityPub.Federator.Transformer.handle_incoming(original_data)
+
+      assert {:ok, original_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(original)
+               |> repo().maybe_preload([:post_content, :media])
+
+      # Verify original post has 2 attachments
+      assert length(original_post.media) == 2
+      original_urls = original_post.media |> Enum.map(& &1.path) |> Enum.sort()
+      assert "https://example.com/image1.jpg" in original_urls
+      assert "https://example.com/image2.png" in original_urls
+
+      # Now create Update activity that:
+      # - Adds a primary image
+      # - Removes image1.jpg 
+      # - Keeps image2.png
+      # - Adds a new image3.gif
+
+      initial_primary_image = %{
+        "type" => "Document",
+        "mediaType" => "image/jpeg",
+        "url" => "https://example.com/primary.jpg",
+        "name" => "Primary image"
+      }
+
+      third_image = %{
+        "type" => "Document",
+        "mediaType" => "image/gif",
+        "url" => "https://example.com/image3.gif",
+        "name" => "Third image"
+      }
+
+      updated_object =
+        original_data
+        |> Map.put("content", "Updated post with different media")
+        |> Map.put("image", initial_primary_image)
+        |> Map.put("attachment", [
+          # Remove first image
+          # Keep second image
+          second_image,
+          # Add new image
+          third_image
+        ])
+
+      update_data = %{
+        "type" => "Update",
+        "id" => "#{original_data["id"]}/update",
+        "actor" => original_data["attributedTo"],
+        "object" => updated_object
+      }
+
+      {:ok, update} = ActivityPub.Federator.Transformer.handle_incoming(update_data)
+
+      assert {:ok, updated_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(update)
+               |> repo().maybe_preload([:post_content, :media])
+
+      # Verify content was updated
+      assert updated_post.post_content.html_body =~ "Updated post with different media"
+
+      # Verify updated post has 3 attachments total (primary + 2 attachments)
+      assert length(updated_post.media) == 3
+
+      updated_urls = updated_post.media |> Enum.map(& &1.path)
+
+      # Should have primary image
+      assert "https://example.com/primary.jpg" in updated_urls
+
+      # Should keep image2.png
+      assert "https://example.com/image2.png" in updated_urls
+
+      # Should have new image3.gif
+      assert "https://example.com/image3.gif" in updated_urls
+
+      # Should NOT have image1.jpg (removed)
+      refute "https://example.com/image1.jpg" in updated_urls
+
+      # Check which image is marked as primary
+      {primary_image, other_images} = Bonfire.Files.split_primary_image(updated_post.media)
+      assert primary_image.path == "https://example.com/primary.jpg"
+      assert length(other_images) == 2
+
+      # Third step: Replace the primary image with a *new* one
+
+      fourth_image = %{
+        "type" => "Document",
+        "mediaType" => "image/gif",
+        "url" => "https://example.com/image4.gif",
+        "name" => "Fourth image"
+      }
+
+      second_update_object =
+        updated_object
+        |> Map.put("content", "New update with new image as primary")
+        # Move image2 to primary
+        |> Map.put("image", fourth_image)
+        # Keep existing attachments
+        |> Map.put("attachment", [
+          initial_primary_image,
+          third_image
+        ])
+
+      second_update_data = %{
+        "type" => "Update",
+        "id" => "#{original_data["id"]}/update2",
+        "actor" => original_data["attributedTo"],
+        "object" => second_update_object
+      }
+
+      {:ok, second_update} = ActivityPub.Federator.Transformer.handle_incoming(second_update_data)
+
+      assert {:ok, final_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(second_update)
+               |> repo().maybe_preload([:post_content, :media])
+
+      # Verify content was updated again
+      assert final_post.post_content.html_body =~ "New update with new image as primary"
+
+      # Verify final post still has 3 attachments 
+      assert length(final_post.media) == 3
+
+      final_urls = final_post.media |> Enum.map(& &1.path)
+
+      # Check which image is marked as primary in the final update
+      {primary_image, final_other_images} = Bonfire.Files.split_primary_image(final_post.media)
+      assert primary_image.path == "https://example.com/image4.gif"
+
+      # Verify the other images are now attachments
+      final_other_urls = final_other_images |> Enum.map(& &1.path)
+      # old primary still there as a normal attachment
+      assert "https://example.com/primary.jpg" in final_other_urls
+      # Should keep image3.gif as attachment
+      assert "https://example.com/image3.gif" in final_other_urls
+
+      # FIXME: Final step: Make the *existing* second_image the primary image (move from attachment to primary)
+      # third_update_object = 
+      #   second_update_data
+      #   |> Map.put("content", "Final update with image2 as primary")
+      #   |> Map.put("image", second_image)  # Move image2 to primary
+      #     # Keep existing attachments
+      #   |> Map.put("attachment", [
+      #     initial_primary_image, 
+      #     third_image,
+      #     fourth_image
+      #   ])
+
+      # third_update_data = %{
+      #   "type" => "Update",
+      #   "id" => "#{original_data["id"]}/update2",
+      #   "actor" => original_data["attributedTo"],
+      #   "object" => third_update_object
+      # }
+
+      # {:ok, third_update} = ActivityPub.Federator.Transformer.handle_incoming(third_update_data)
+
+      # assert {:ok, final_post} =
+      #          Bonfire.Federate.ActivityPub.Incoming.receive_activity(third_update)
+      #          |> repo().maybe_preload([:post_content, :media])
+
+      #          # Verify content was updated again
+      # assert final_post.post_content.html_body =~ "Final update with image2 as primary"
+
+      # # Verify final post still has 3 attachments 
+      # assert length(final_post.media) == 3
+
+      # final_urls = final_post.media |> Enum.map(&(&1.path)) 
+
+      # # Check which image is marked as primary in the final update
+      # {primary_image, final_other_images} = Bonfire.Files.split_primary_image(final_post.media)
+      # assert primary_image.path == "https://example.com/image2.png"
+      # assert length(final_other_images) == 2
+
+      # # Verify the other images are now attachments
+      # final_other_urls = final_other_images |> Enum.map(&(&1.path)) 
+      # # old primary still there as a normal attachment
+      # assert "https://example.com/primary.jpg" in final_other_urls
+      # # Should keep other images as attachment
+      # assert "https://example.com/image3.gif" in final_other_urls
+      # assert "https://example.com/image4.gif" in final_other_urls
     end
   end
 end
--- test/activity_pub_integration/post_web_test.exs
+++ test/activity_pub_integration/post_web_test.exs
@@ -94,59 +94,26 @@ defmodule Bonfire.Federate.ActivityPub.PostWebTest do
              |> redirected_to() =~ "/pub/objects/#{post.id}"
     end
 
-    test "process pixelfed activity with image" do
-      data =
-        "../fixtures/pixelfed-image.json"
-        |> Path.expand(__DIR__)
-        |> File.read!()
-        |> Jason.decode!()
-
-      # |> debug("pxxx")
-
-      {:ok, data} = ActivityPub.Federator.Transformer.handle_incoming(data)
-
-      assert {:ok, post} =
-               Bonfire.Federate.ActivityPub.Incoming.receive_activity(data)
-               |> repo().maybe_preload([:post_content, :media])
-
-      assert post.__struct__ == Bonfire.Data.Social.Post
-      # debug(post)
-      assert is_binary(e(post, :post_content, :html_body, nil))
-
-      assert match?(
-               %{
-                 media: [
-                   %Bonfire.Files.Media{
-                     path:
-                       "https://pxscdn.com/public/m/_v2/411/7198ec0c0-99bc91/6CWmVqUJS5Rx/cXZwkROZAkUOQEidxDNxZYlezi5nRBBLy5f2YAm0.jpg"
-                   }
-                 ]
-               },
-               post
-             )
-
-      # assert doc =
-      #          render_stateful(Bonfire.UI.Social.ActivityLive, %{
-      #            id: "activity",
-      #            object: post
-      #          })
-
-      # assert doc
-      #        |> debug
-    end
-
     test "process mastodon activity with content warning" do
       # url = "https://mocked.local/users/karen/statuses/114800379424129152/activity"
 
       data =
-        "../fixtures/mastodon-post-activity-with-cw.json"
+        "../fixtures/mastodon-post-activity.json"
         |> Path.expand(__DIR__)
         |> File.read!()
         |> Jason.decode!()
 
       # |> debug("pxxx")
 
-      {:ok, data} = ActivityPub.Federator.Transformer.handle_incoming(data)
+      {:ok, data} =
+        data
+        |> Map.update("object", %{}, fn object ->
+          object
+          # ensure sensitive is set to true
+          |> Map.put("sensitive", true)
+          |> Map.put("summary", "politics")
+        end)
+        |> ActivityPub.Federator.Transformer.handle_incoming()
 
       assert {:ok, post} =
                Bonfire.Federate.ActivityPub.Incoming.receive_activity(data)
@@ -161,6 +128,260 @@ defmodule Bonfire.Federate.ActivityPub.PostWebTest do
 
       # Post should be marked as sensitive when it has a content warning
       assert post.sensitive.is_sensitive == true
+    end
+
+    test "process mastodon Update activity with content warning should preserve CW" do
+      # First, create the original post without CW
+      original_data =
+        "../fixtures/mastodon-post-activity.json"
+        |> Path.expand(__DIR__)
+        |> File.read!()
+        |> Jason.decode!()
+
+      {:ok, original} = ActivityPub.Federator.Transformer.handle_incoming(original_data)
+
+      assert {:ok, original_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(original)
+               |> repo().maybe_preload([:post_content, :sensitive])
+
+      # Verify original post has no CW
+      assert is_nil(e(original_post, :post_content, :summary, nil)) or
+               e(original_post, :post_content, :summary, nil) == ""
+
+      assert original_post.sensitive.is_sensitive == false
+
+      # Now process an Update activity that adds a content warning
+      {:ok, update_data} =
+        original_data
+        # Ensure this is an Update activity
+        |> Map.put("type", "Update")
+        |> Map.update("object", %{}, fn object ->
+          object
+          |> Map.put("sensitive", true)
+          |> Map.put("summary", "politics")
+        end)
+        |> Map.update("id", %{}, fn id ->
+          # needs a unique activity ID
+          "#{id}/update"
+        end)
+        |> ActivityPub.Federator.Transformer.handle_incoming()
+
+      assert {:ok, updated_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(update_data)
+               |> repo().maybe_preload([:post_content, :sensitive])
+
+      #  |> debug("received_update")
+
+      # This should pass when the bug is fixed
+      # Check that content warning/summary is preserved after update
+      s = e(updated_post, :post_content, :summary, nil)
+      assert is_binary(s) and s != "", "Content warning text should be added by Update activity"
+
+      # Post should be marked as sensitive when CW is added via Update
+      assert updated_post.sensitive.is_sensitive == true,
+             "Post should be marked sensitive after Update with CW"
+    end
+
+    test "process mastodon Update activity changing content warning" do
+      # First, create the original post WITH CW
+      original_data =
+        "../fixtures/mastodon-post-activity.json"
+        |> Path.expand(__DIR__)
+        |> File.read!()
+        |> Jason.decode!()
+        |> Map.update("object", %{}, fn object ->
+          object
+          |> Map.put("sensitive", true)
+          |> Map.put("summary", "politics")
+        end)
+
+      {:ok, original} = ActivityPub.Federator.Transformer.handle_incoming(original_data)
+
+      assert {:ok, original_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(original)
+               |> repo().maybe_preload([:post_content, :sensitive])
+
+      # Verify original post HAS a CW
+      s = e(original_post, :post_content, :summary, nil)
+      assert is_binary(s) and s != "", "Original post should have content warning"
+      assert original_post.sensitive.is_sensitive == true
+
+      # Now process an Update activity that removes the content warning
+      {:ok, update_data} =
+        original_data
+        # Ensure this is an Update activity
+        |> Map.put("type", "Update")
+        |> Map.update("object", %{}, fn object ->
+          object
+          |> Map.put("sensitive", false)
+          # Remove the summary/CW
+          |> Map.delete("summary")
+        end)
+        |> Map.update("id", %{}, fn id ->
+          # needs a unique activity ID
+          "#{id}/update"
+        end)
+        |> ActivityPub.Federator.Transformer.handle_incoming()
+
+      assert {:ok, updated_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(update_data)
+               |> repo().maybe_preload([:post_content, :sensitive])
+
+      #  |> debug("received_update")
+
+      # Check that content warning/summary is removed after update
+      s = e(updated_post, :post_content, :summary, nil)
+      assert is_nil(s) or s == "", "Content warning should be removed in Update activity"
+
+      # Post should no longer be marked as sensitive when CW is removed via Update
+      assert e(updated_post, :sensitive, :is_sensitive, false) == false,
+             "Post should not be marked sensitive after Update removes CW"
+    end
+
+    test "process mastodon Update activity with hashtag and mention changes" do
+      # First, create the original post with existing hashtags
+      original_data =
+        "../fixtures/mastodon-post-activity.json"
+        |> Path.expand(__DIR__)
+        |> File.read!()
+        |> Jason.decode!()
+
+      {:ok, original} = ActivityPub.Federator.Transformer.handle_incoming(original_data)
+      # |> debug("normalized")
+
+      assert {:ok, original_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(original)
+               |> repo().maybe_preload([:post_content, tags: [:named, :character]])
+
+      # Verify original post has the fixture hashtags: #first_hashtag, #test, #tests
+      original_hashtags =
+        original_post.tags
+        |> Enum.filter(&is_nil(e(&1, :character, nil)))
+        |> Enum.map(&e(&1, :named, :name, nil))
+        |> Enum.sort()
+
+      assert "first_hashtag" in original_hashtags
+      assert "test" in original_hashtags
+      assert "tests" in original_hashtags
+      assert length(original_hashtags) == 3
+
+      # Verify original post has no mentions
+      original_mentions =
+        original_post.tags
+        |> Enum.filter(&(not is_nil(e(&1, :character, nil))))
+
+      assert length(original_mentions) == 0
+
+      # First Update: Add new hashtag, remove one, keep others, add mentions
+      {:ok, update_data} =
+        original_data
+        |> Map.put("type", "Update")
+        |> Map.update("object", %{}, fn object ->
+          object
+          |> Map.put("content", "Updated content with different hashtags and @karen mention")
+          |> Map.put("tag", [
+            # Keep #first_hashtag and #test, remove #tests
+            %{
+              "type" => "Hashtag",
+              "name" => "#first_hashtag"
+            },
+            %{
+              "type" => "Hashtag",
+              "name" => "#test"
+            },
+            # Add new hashtag
+            %{
+              "type" => "Hashtag",
+              "name" => "#elixir"
+            },
+            # Add mention
+            %{
+              "type" => "Mention",
+              "href" => @remote_actor,
+              "name" => "@karen"
+            }
+          ])
+        end)
+        |> Map.update("id", %{}, fn id ->
+          # needs a unique activity ID
+          "#{id}/update"
+        end)
+        |> ActivityPub.Federator.Transformer.handle_incoming()
+
+      # |> debug("normalized")
+
+      assert {:ok, updated_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(update_data)
+               |> repo().maybe_preload([:post_content, tags: [:named, :character]])
+
+      # Verify updated post has correct hashtags
+      updated_hashtags =
+        updated_post.tags
+        |> Enum.filter(&is_nil(e(&1, :character, nil)))
+        |> Enum.map(&e(&1, :named, :name, nil))
+        |> Enum.sort()
+
+      # kept
+      assert "first_hashtag" in updated_hashtags
+      # kept
+      assert "test" in updated_hashtags
+      # added
+      assert "elixir" in updated_hashtags
+      # removed
+      refute "tests" in updated_hashtags
+      assert length(updated_hashtags) == 3
+
+      # Verify updated post has correct mentions
+      updated_mentions =
+        updated_post.tags
+        |> Enum.filter(&(not is_nil(e(&1, :character, nil))))
+
+      assert length(updated_mentions) == 1
+
+      # Second Update: Remove all hashtags but keep mentions, add new mention
+      {:ok, second_update_data} =
+        original_data
+        |> Map.put("type", "Update")
+        |> Map.update("object", %{}, fn object ->
+          object
+          |> Map.put("content", "Final update with no hashtags but @karen mention")
+          |> Map.put("tag", [
+            # Keep existing mention
+            %{
+              "type" => "Mention",
+              "href" => @remote_actor,
+              "name" => "@karen"
+            }
+            # Remove all hashtags
+          ])
+        end)
+        |> Map.update("id", %{}, fn id ->
+          # needs a unique activity ID
+          "#{id}/update2"
+        end)
+        |> ActivityPub.Federator.Transformer.handle_incoming()
+
+      assert {:ok, final_post} =
+               Bonfire.Federate.ActivityPub.Incoming.receive_activity(second_update_data)
+               |> repo().maybe_preload([:post_content, tags: [:named, :character]])
+
+      # Verify final post has no hashtags
+      final_hashtags =
+        final_post.tags
+        |> Enum.filter(&is_nil(e(&1, :character, nil)))
+
+      assert length(final_hashtags) == 0
+
+      # Verify final post still has mentions
+      final_mentions =
+        final_post.tags
+        |> Enum.filter(&(not is_nil(e(&1, :character, nil))))
+
+      assert length(final_mentions) == 1
+
+      # Verify content was updated
+      assert final_post.post_content.html_body =~
+               "Final update with no hashtags but @karen mention"
     end
   end
 end
