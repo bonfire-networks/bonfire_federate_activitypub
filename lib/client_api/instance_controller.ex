5ae366b969039b345d5152a30134b4fec50f3f61
Mayel de Borniol <git@mayel.space>
2025-08-07 20:29:39 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1476

--- lib/client_api/instance_controller.ex
+++ lib/client_api/instance_controller.ex
@@ -21,16 +21,11 @@ defmodule Bonfire.API.MastoCompatible.InstanceController do
       "short_description" => Config.get([:ui, :theme, :instance_tagline], nil),
       "description" => Config.get([:ui, :theme, :instance_description], nil),
       "email" => "no-reply@no-reply.net",
-
-      # TODO
-      "languages" => [
-        "en"
-      ],
+      "languages" => Bonfire.Common.Localise.known_locales(),
       # TODO
       "rules" => [],
       "registrations" => !Bonfire.Me.Accounts.instance_is_invite_only?(),
-      # TODO
-      "invites_enabled" => false,
+      "invites_enabled" => Bonfire.Me.Accounts.instance_is_invite_only?(),
       # TODO
       "approval_required" => false,
       "uri" => base_url,
@@ -38,69 +33,44 @@ defmodule Bonfire.API.MastoCompatible.InstanceController do
         "2.7.2 (compatible; Akkoma 3.9.3; #{app_name} #{Bonfire.Application.version()})",
       "source_url" => Bonfire.Application.repository(),
       # non-standard
-      "federating" => ActivityPub.Config.federating?(),
+      "federating" => ActivityPub.Config.federating?() || false,
       "configuration" => %{
         "urls" =>
           %{
             # "streaming"=> "wss://#{base_url}"
           },
         "vapid" => %{
+          # TODO
           "public_key" => nil
         },
         "accounts" => %{
-          "max_featured_tags" => 10,
-          "max_pinned_statuses" => 4
+          # TODO
+          "max_featured_tags" => 1,
+          "max_pinned_statuses" => 1
         },
         "statuses" => %{
+          # TODO
           "max_characters" => 500_000,
           "max_media_attachments" => 20,
           "characters_reserved_per_url" => 3
         },
         "media_attachments" => %{
-          #  TODO
-          "supported_mime_types" => [
-            "image/jpeg",
-            "image/png",
-            "image/gif",
-            "image/heic",
-            "image/heif",
-            "image/webp",
-            "video/webm",
-            "video/mp4",
-            "video/quicktime",
-            "video/ogg",
-            "audio/wave",
-            "audio/wav",
-            "audio/x-wav",
-            "audio/x-pn-wave",
-            "audio/vnd.wave",
-            "audio/ogg",
-            "audio/vorbis",
-            "audio/mpeg",
-            "audio/mp3",
-            "audio/webm",
-            "audio/flac",
-            "audio/aac",
-            "audio/m4a",
-            "audio/x-m4a",
-            "audio/mp4",
-            "audio/3gpp",
-            "video/x-ms-asf"
-          ],
-          "image_size_limit" => 10_485_760,
+          "supported_mime_types" => Bonfire.Files.MimeTypes.supported_media() |> Map.keys(),
+          "image_size_limit" => Bonfire.Files.ImageUploader.max_file_size(),
+          "video_size_limit" => Bonfire.Files.VideoUploader.max_file_size(),
+          # TODO
           "image_matrix_limit" => 16_777_216,
-          "video_size_limit" => 41_943_040,
-          "video_frame_rate_limit" => 60,
-          "video_matrix_limit" => 2_304_000
+          "video_matrix_limit" => 2_304_000,
+          "video_frame_rate_limit" => 60
         },
         "polls" => %{
+          # TODO
           "max_options" => 100,
           "max_characters_per_option" => 50_000,
           "min_expiration" => 60,
           "max_expiration" => 31_536_000
         },
         "translation" => %{
-          # TODO
           "enabled" => false
         }
       }
